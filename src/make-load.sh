f03fe9d91080180da56279828cc4b2ecf10191a0
Begerad, Stefan <dialog@swingbe.de>
2023-11-03 16:48:38 +0100

    feat: add src/make-load.sh

---
+++ src/make-load.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+echo "Started..."
+#Started...
+#
+# special variable $# is the number of arguments
+if [ $# -lt 5 ] ; then
+    echo 'Call ./<script> <db name> <db user> <db schema> <make file path> <work dir>'
+    exit 1
+fi
+#
+DB_NAME="$1"
+echo "DB_NAME: $DB_NAME"
+DB_USER="$2"
+echo "DB_USER: $DB_USER"
+DB_SCHEMA="$3"
+echo "DB_SCHEMA: ${DB_SCHEMA}"
+MAKE_PATH="$4"
+echo "MAKE_PATH: ${MAKE_PATH}"
+WORK_DIR="$5"
+echo "WORK_DIR: $WORK_DIR"
+#
+make -C $MAKE_PATH -f Makefile HOST=localhost PORT=5432 USER=$DB_USER DB=$DB_NAME SCHEMA=$DB_SCHEMA GTFS=${WORK_DIR} load
+#
+echo "Done."
+#done.
