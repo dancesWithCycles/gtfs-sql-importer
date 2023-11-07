31bb52d2d3c4a7f15c920d808148e6f75fd2de79
Begerad, Stefan <dialog@swingbe.de>
2023-11-07 17:35:32 +0100

    feat: add src/download-from-mobilithek.sh

---
+++ src/download-from-mobilithek.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+echo "Started..."
+#Started...
+#
+# special variable $# is the number of arguments
+if [ $# -lt 2 ] ; then
+    echo 'Call ./<script> <url> <cert secret>'
+    exit 1
+fi
+#
+downloadUrl="$1"
+echo "downloadUrl: $downloadUrl"
+#
+certSecret="$2"
+echo "certSecret: $certSecret"
+#
+cd ~
+rm -r ${HOME}/gtfs
+mkdir -p ${HOME}/gtfs
+curl --request GET --url $downloadUrl --header "Accept-Encoding: gzip, deflate"  --cert-type P12 --cert $HOME/certificate.p12:"$certSecret" --output ~/gtfs/gtfs.gz
+#
+echo "Done."
+#done.
